6a2e9d4f2ab65bd0cfc85b559496811baecc8d23
Roeckoe <sarahmbuyi13@gmail.com>
2017-01-05 11:27:06 +0100

    Datei bearbeitet

---
+++ bot-python.py
@@ -0,0 +1,55 @@
+s = input ("Hallo :")
+mensch = input ("> " + s + "\n* ")
+
+s = input ("Ich möchte mich mit dir unterhalten :")
+
+s = input ("Gibt es etwas, dass du mit absoluter Leidenschaft tust? :")
+if "ja" in s:
+    print("hört sich ja gut an")
+elif "nein" in s:
+    print("schade")
+
+s = input ("Was machst du so? :")
+if "ich esse gerade" in s:
+    print ("guten Appetit")
+elif "ich mache nichts" in s:
+    print ("ohh")
+
+s = input ("Magst du irgendwelche Serien? :")
+if "Ja" in s:
+    print ("Ich auch")
+elif "nein" in s:
+    print ("du bist aber langweilig")
+
+s = input ("Wie war eigentlich dein Urlaub?  :")
+if "Sehr gut" in s:
+    print ("Freut mich.Meiner auch.")
+elif "nicht so gut" in s:
+    print ("tja, pech gehabt")
+
+s = input ("Tschüss :")
+if "tschüss" in s:
+    print ("bye")
+    sys. exit ()
+elif "wieso" in s:
+    print ("Darum")
+    sys. exit ()
+
+    
+if "Ciao" in mensch:
+    s = input ("Kannst du nicht einmal Tschüss sagen")
+
+elif "Ja" in mensch:
+    s = input ("Absolut")
+else:
+    "Ok" in mensch
+    s = input ("Schade")
+
+print ("Schön mit dir geredet zu haben!")
+
+    
+
+
+
+
+
