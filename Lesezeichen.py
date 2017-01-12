d6913a25220d40cb91d3250dabfb798f9b43f972
Roeckoe <sarahmbuyi13@gmail.com>
2017-01-12 11:06:40 +0100

    Add files via upload

---
+++ Lesezeichen.py
@@ -0,0 +1,18 @@
+x = 0
+
+# Funktion, die 3 Ausgaben macht
+def function():
+	global x
+	x = x + 1
+	
+	if (lesezeichen == 1):
+		print("Hallo")
+	elif (lesezeichen == 2) :
+                print("das sehe ich auch so.")
+        else:(lesezeichen == 3) :
+                print("Auf Wiedersehen")
+
+# rufe die Funktion 1x auf.
+function()
+function()
+function()
